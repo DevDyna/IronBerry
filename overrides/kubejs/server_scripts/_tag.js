66ffb7804c8dbf29636722b5d2f9aa1004cab5c1
DevDyna <78147916+DevDyna@users.noreply.github.com>
2023-04-22 00:57:04 +0200

    Delete overrides/kubejs directory

--- overrides/kubejs/server_scripts/_tag.js
+++
@@ -1,5 +0,0 @@
-ServerEvents.tags('item', event => {
-
-event.get('reactive:body_sources').removeAll()
-event.get('reactive:caustic_sources').removeAll()
-})
--- overrides/kubejs/startup_scripts/script.js
+++
@@ -1,4 +0,0 @@
-StartupEvents.registry('block', event => {
-	// Register new blocks here
-	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
-})
