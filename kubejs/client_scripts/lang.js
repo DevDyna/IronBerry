3c1c37cac2934a1d941750404d003e53aa7d1c02
DevDyna <andrearisorti2003@gmail.com>
2023-10-16 21:23:01 +0200

    added custom gas lang and gateway

---
+++ kubejs/client_scripts/lang.js
@@ -0,0 +1,6 @@
+ClientEvents.highPriorityAssets(event => {  
+ 
+  event.addLang('gas.kubejs.methane', 'Methane Gas')
+  event.addLang('gateways.simple_gate','Tutorial of how to die by')
+  event.addLang('gateways.battle_gate_large','Bosses')
+})
