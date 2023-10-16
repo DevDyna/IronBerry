7165af7dc8a387fe38ee52d9cfe5fb2ce5848c99
DevDyna <andrearisorti2003@gmail.com>
2023-10-16 21:02:06 +0200

    added base upgrades and trophy blanks

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -9,106 +9,115 @@ StartupEvents.registry('item', event => {
     .texture('ironberry:item/rose')
     .displayName('Rose')
 
-        //gems
-        event.create('alpha')
-        .texture('ironberry:item/gems/alpha')
-        .displayName('Alpha')
+    //gems
+    event.create('alpha')
+    .texture('ironberry:item/gems/alpha')
+    .displayName('Alpha')
 
-        event.create('beta')
-        .texture('ironberry:item/gems/beta')
-        .displayName('Beta')
+    event.create('beta')
+    .texture('ironberry:item/gems/beta')
+    .displayName('Beta')
 
-        event.create('aquamarine')
-        .texture('ironberry:item/gems/aquamarine')
-        .displayName('Aquamarine')
+    event.create('aquamarine')
+    .texture('ironberry:item/gems/aquamarine')
+    .displayName('Aquamarine')
 
-        event.create('jade')
-        .texture('ironberry:item/gems/jade')
-        .displayName('Jade')
+    event.create('jade')
+    .texture('ironberry:item/gems/jade')
+    .displayName('Jade')
 
-        event.create('iolite')
-        .texture('ironberry:item/gems/iolite')
-        .displayName('Iolite')
+    event.create('iolite')
+    .texture('ironberry:item/gems/iolite')
+    .displayName('Iolite')
 
-        event.create('topaz')
-        .texture('ironberry:item/gems/topaz')
-        .displayName('Topaz')
+    event.create('topaz')
+    .texture('ironberry:item/gems/topaz')
+    .displayName('Topaz')
 
+    //grave guardian spawn
+    event.create('grave_spawn')
+    .texture('ironberry:item/grave_spawn_egg')
+    .displayName('Grave Guardian Spawn Egg')
 
-
-        //grave guardian spawn
-        event.create('grave_spawn')
-        .texture('ironberry:item/grave_spawn_egg')
-        .displayName('Grave Guardian Spawn Egg')
-
-
-	    event.create('scroll')
+    event.create('scroll')
     .texture('ironberry:item/scroll')
-	.displayName('Null Scroll')
-	
-		    event.create('omni')
+    .displayName('Null Scroll')
+
+    event.create('omni')
     .texture('ironberry:item/omni')
-	.displayName('OmniTablet')
+    .displayName('OmniTablet')
 
-		    event.create('iron_rod')
+    event.create('iron_rod')
     .texture('ironberry:item/iron_rod')
-	.displayName('Iron Rod')
-	
-			    event.create('soul')
-    .texture('ironberry:item/soul')
-	.displayName('Soul jar')
-					
-					
-	event.create('ice_shard')
-	.texture('ironberry:item/ice_shard')
-	.displayName('Ice Shard')
-	
-	event.create('ore_bone_meal')
-	.texture('ironberry:item/ore_bone_meal')
-	.displayName('Mineral Fertilizer')
-	
-	event.create('rich_bone_meal')
-	.texture('ironberry:item/rich_bone_meal')
-	.displayName('Rich Fertilizer')
+    .displayName('Iron Rod')
 
-	//temporarly added , maybe on a future will removed
-	event.create('illuminati')
-	.texture('ironberry:item/illuminati_yota')
-	.displayName('Illuminati Yota')
-	
-	event.create('pre_compressed')
-	.texture('ironberry:item/pre_compressed')
-	.displayName('Precompressed Iron Ingot')
-	
-	event.create('scroll_safety')
-	.texture('ironberry:item/scroll_a')
-	.displayName('Scroll of Safety')
-	
-	event.create('scroll_discontinuity')
-	.texture('ironberry:item/scroll_b')
-	.displayName('Scroll of Discontinuity')
-	
-	
-	event.create('raw_iron_nugget')
-	.texture('ironberry:item/raw_iron_nugget')
-	.displayName('Raw Iron Nugget')
-	
-		event.create('raw_gold_nugget')
-	.texture('ironberry:item/raw_gold_nugget')
-	.displayName('Raw Gold Nugget')
-	
-		event.create('raw_copper_nugget')
-	.texture('ironberry:item/raw_copper_nugget')
-	.displayName('Raw Copper Nugget')
-	
-	
-			event.create('processor')
-	.texture('ironberry:item/processor')
-	.displayName('Menril MicroProcessor Chip')
-	
-		event.create('ritual_tablet')
+    event.create('soul')
+    .texture('ironberry:item/soul')
+    .displayName('Soul jar')
+
+    event.create('ice_shard')
+    .texture('ironberry:item/ice_shard')
+    .displayName('Ice Shard')
+
+    event.create('ore_bone_meal')
+    .texture('ironberry:item/ore_bone_meal')
+    .displayName('Mineral Fertilizer')
+
+    event.create('rich_bone_meal')
+    .texture('ironberry:item/rich_bone_meal')
+    .displayName('Rich Fertilizer')
+
+    //temporarly added , maybe on a future will removed
+    event.create('illuminati')
+    .texture('ironberry:item/illuminati_yota')
+    .displayName('Illuminati Yota')
+
+    event.create('pre_compressed')
+    .texture('ironberry:item/pre_compressed')
+    .displayName('Precompressed Iron Ingot')
+
+    event.create('scroll_safety')
+    .texture('ironberry:item/scroll_a')
+    .displayName('Scroll of Safety')
+
+    event.create('scroll_discontinuity')
+    .texture('ironberry:item/scroll_b')
+    .displayName('Scroll of Discontinuity')
+
+    event.create('raw_iron_nugget')
+    .texture('ironberry:item/raw_iron_nugget')
+    .displayName('Raw Iron Nugget')
+
+    event.create('raw_gold_nugget')
+    .texture('ironberry:item/raw_gold_nugget')
+    .displayName('Raw Gold Nugget')
+
+    event.create('raw_copper_nugget')
+    .texture('ironberry:item/raw_copper_nugget')
+    .displayName('Raw Copper Nugget')
+
+    event.create('processor')
+    .texture('ironberry:item/processor')
+    .displayName('Menril MicroProcessor Chip')
+
+    event.create('ritual_tablet')
     .texture('ironberry:item/ritual_tablet')
-	.displayName(' Empty Ritual Tablet')
+    .displayName(' Empty Ritual Tablet')
 	
+	    event.create('boss_trophy')
+    .texture('ironberry:item/boss_trophy')
+    .displayName('Boss Trophy')
 	
+		    event.create('miniboss_trophy')
+    .texture('ironberry:item/miniboss_trophy')
+    .displayName('MiniBoss Trophy')
+
+		    event.create('speed_base')
+    .texture('ironberry:item/speed_base')
+    .displayName('Speed Upgrade Base')
+
+		    event.create('quantity_base')
+    .texture('ironberry:item/quantity_base')
+    .displayName('Quantity Upgrade Base')
+
 })
