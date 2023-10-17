89ae34a3f24481b027c91f11ba6ea9721aef33ad
DevDyna <andrearisorti2003@gmail.com>
2023-10-18 00:13:29 +0200

    hidden all gasses

--- kubejs/client_scripts/jei.js
+++ kubejs/client_scripts/jei.js
@@ -23,7 +23,7 @@ JEIEvents.hideFluids(event => {
  
 const $MekanismJEI = Java.loadClass('mekanism.client.jei.MekanismJEI')
 JEIEvents.hideCustom(event => {
-  //event.get($MekanismJEI.TYPE_GAS).hideAll()
+  event.get($MekanismJEI.TYPE_GAS).hideAll()
   event.get($MekanismJEI.TYPE_INFUSION).hideAll()
   event.get($MekanismJEI.TYPE_PIGMENT).hideAll()
   event.get($MekanismJEI.TYPE_SLURRY).hideAll()
