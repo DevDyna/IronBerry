f2f2a5127d19f84b462cc2351d1ea180ef03bbde
DevDyna <andrearisorti2003@gmail.com>
2023-10-18 23:12:27 +0200

    formatted start.block

--- kubejs/startup_scripts/modify/blocks.js
+++ kubejs/startup_scripts/modify/blocks.js
@@ -1,8 +1,10 @@
-BlockEvents.modification( event => {
-	
-event.modify('craftingstation:crafting_station', block => {block.destroySpeed = 0.5})
-event.modify('craftingstation:crafting_station_slab', block => {block.destroySpeed = 0.5})
+BlockEvents.modification(event => {
 
-
+    event.modify('craftingstation:crafting_station', block => {
+        block.destroySpeed = 0.5
+    })
+    event.modify('craftingstation:crafting_station_slab', block => {
+        block.destroySpeed = 0.5
+    })
 
 })
