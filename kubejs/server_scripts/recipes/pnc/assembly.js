cb379857e1237d92265ab07c2793b03802a8373c
DevDyna <andrearisorti2003@gmail.com>
2023-11-01 19:58:24 +0100

    tweakered easter egg

---
+++ kubejs/server_scripts/recipes/pnc/assembly.js
@@ -0,0 +1,37 @@
+ServerEvents.recipes(event => {
+
+    event.custom({
+        "type": "pneumaticcraft:assembly_drill",
+        "input": {
+            "item": "supplementaries:present"
+        },
+        "program": "drill",
+        "result": {
+            "item": "tombstone:gift",
+            "nbt": {
+				Items:[
+				{Count:1,id:"tombstone:gift",tag:{
+					Items:[
+					{Count:1,id:"tombstone:gift",tag:{
+						Items:[
+						{Count:1,id:"tombstone:gift",tag:{
+							Items:[
+							{Count:1,id:"tombstone:gift",tag:{
+								Items:[
+								{Count:1,id:"tombstone:gift",tag:{
+									Items:[
+									{Count:1,id:"tombstone:gift",tag:{
+										Items:[
+										{Count:1,id:"tombstone:gift",tag:{
+											Items:[
+											{Count:1,id:"tombstone:gift",tag:{
+												Items:[
+												{Count:1,id:"tombstone:gift",tag:{
+													Items:[
+													{Count:1,id:"tombstone:gift",tag:{
+														Items:[
+														{Count:1,id:"minecraft:player_head",tag:{SkullOwner:"DevDyna"}}]}}]}}]}}]}}]}}]}}]}}]}}]}}]}}]}
+        }
+    })
+
+})
