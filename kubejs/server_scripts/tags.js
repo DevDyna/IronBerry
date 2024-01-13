fc06160977ecaf0570d78b3cbe42c9a6ba929cd5
DevDyna <andrearisorti2003@gmail.com>
2024-01-14 00:48:47 +0100

    Update tags.js

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -694,6 +694,7 @@ ServerEvents.tags('item', event => {
 
 	event.add('ironberry:source_gems',['minecraft:lapis_lazuli', 'minecraft:amethyst_shard'])
 
+	event.add('ironberry:patina_source',    ['minecraft:oxidized_copper', 'minecraft:oxidized_cut_copper', 'minecraft:oxidized_cut_copper_stairs', 'minecraft:oxidized_cut_copper_slab', 'quark:oxidized_cut_copper_vertical_slab', 'minecraft:weathered_copper', 'minecraft:weathered_cut_copper', 'minecraft:weathered_cut_copper_stairs', 'minecraft:weathered_cut_copper_slab', 'quark:weathered_cut_copper_vertical_slab', 'minecraft:exposed_copper', 'minecraft:exposed_cut_copper', 'minecraft:exposed_cut_copper_stairs', 'minecraft:exposed_cut_copper_slab', 'quark:exposed_cut_copper_vertical_slab'])
 
 })
 
