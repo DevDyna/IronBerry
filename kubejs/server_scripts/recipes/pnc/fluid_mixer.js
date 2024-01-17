a2135c8a52007cbc7d7b6a7477c222733d93dacf
DevDyna <andrearisorti2003@gmail.com>
2024-01-18 00:47:59 +0100

    Create fluid_mixer.js

---
+++ kubejs/server_scripts/recipes/pnc/fluid_mixer.js
@@ -0,0 +1,25 @@
+ServerEvents.recipes(event => {
+	//------------------------------------------------------//
+	event.custom({
+  "type": "pneumaticcraft:fluid_mixer",
+  "input1": {
+	      "type": "pneumaticcraft:fluid",
+    "tag": "ironberry:chad_water",
+    "amount": 1000
+  },
+  "input2": {
+    "type": "pneumaticcraft:fluid",
+    "tag": "minecraft:lava",
+    "amount": 1000
+  },
+  "item_output": {
+    "item": 'kubejs:instability'
+  },
+  "pressure": 2.0,
+  "time": 40
+})
+
+
+//------------------------------------------------------//
+	})
+
