d8466bff593dcdcdd027433bbda86aec4cca9f03
DevDyna <andrearisorti2003@gmail.com>
2024-01-18 23:02:33 +0100

    Update alloy_blue.js

--- kubejs/server_scripts/recipes/bluepower/alloy_blue.js
+++ kubejs/server_scripts/recipes/bluepower/alloy_blue.js
@@ -128,7 +128,7 @@ ServerEvents.recipes(event => {
 	alloy(['kubejs:patina', 'hostilenetworks:twilight_prediction'],[1,1],'hostilenetworks:empty_prediction',1)
 
 	alloy(['pneumaticcraft:plastic', 'nuclearcraft:silicon_gem'],[1,1],'pneumaticcraft:empty_pcb',4)
-
+	
 	//alloy([],)
 
 
