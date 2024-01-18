cb55cf9ec5d1a1898dd86c00107cbe5fabd2b602
DevDyna <andrearisorti2003@gmail.com>
2024-01-18 01:34:00 +0100

    Create thermoplant.js

---
+++ kubejs/server_scripts/recipes/pnc/thermoplant.js
@@ -0,0 +1,44 @@
+ServerEvents.recipes(event => {
+	//------------------------------------------------------//
+	event.custom({
+  "type": "pneumaticcraft:thermo_plant",
+  "fluid_input": {
+    "type": "pneumaticcraft:fluid",
+    "tag": "ironberry:chad_water",
+    "amount": 1000
+  },
+  "item_input": {
+    "item": 'minecraft:cobblestone'
+  },
+  "temperature": {
+    "min_temp": 272
+  },
+  "fluid_output": {
+    "fluid": "minecraft:lava",
+    "amount": 100
+  }
+})
+
+
+//------------------------------------------------------//
+	event.custom({
+  "type": "pneumaticcraft:thermo_plant",
+  "fluid_input": {
+    "type": "pneumaticcraft:fluid",
+    "tag": "ironberry:chad_water",
+    "amount": 1000
+  },
+  "item_input": {
+    "item": 'kubejs:rich_bone_meal'
+  },
+  "temperature": {
+    "min_temp": 272
+  },
+  "fluid_output": {
+    "fluid": "pneumaticcraft:etching_acid",
+    "amount": 100
+  }
+})
+
+	})
+
