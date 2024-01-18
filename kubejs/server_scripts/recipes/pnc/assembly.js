2f73ec0996fee840948f3457183cf83248033b22
DevDyna <andrearisorti2003@gmail.com>
2024-01-18 21:16:29 +0100

    Update assembly.js

--- kubejs/server_scripts/recipes/pnc/assembly.js
+++ kubejs/server_scripts/recipes/pnc/assembly.js
@@ -1,5 +1,5 @@
 ServerEvents.recipes(event => {
-
+//-----------------------------------------------------------------------------------------------------------//
     event.custom({
         "type": "pneumaticcraft:assembly_drill",
         "input": {
@@ -33,5 +33,43 @@ ServerEvents.recipes(event => {
 														{Count:1,id:"minecraft:player_head",tag:{SkullOwner:"DevDyna"}}]}}]}}]}}]}}]}}]}}]}}]}}]}}]}}]}
         }
     })
+//-----------------------------------------------------------------------------------------------------------//
+	    event.custom({
+        "type": "pneumaticcraft:assembly_laser",
+        "input": {
+            "tag": 'minecraft:logs_that_burn'
+        },
+        "program": "laser",
+        "result": {
+            "item": "supplementaries:ash",
+			"count":16
+			}
+    })
+//-----------------------------------------------------------------------------------------------------------//
+	    event.custom({
+        "type": "pneumaticcraft:assembly_drill",
+        "input": {
+            "item": "supplementaries:ash"
+        },
+        "program": "drill",
+        "result": {
+            "item": "minecraft:gold_nugget"
+			}
+    })
+//-----------------------------------------------------------------------------------------------------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })
