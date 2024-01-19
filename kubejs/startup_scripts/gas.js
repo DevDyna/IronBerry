a71f72a8db36e5bac3099fca7d4da93f4e97d25d
DevDyna <andrearisorti2003@gmail.com>
2024-01-20 00:13:47 +0100

    Update gas.js

--- kubejs/startup_scripts/gas.js
+++ kubejs/startup_scripts/gas.js
@@ -3,6 +3,12 @@ const $GasDeferredRegister = Java.loadClass('mekanism.common.registration.impl.G
 
 const GASES = new $GasDeferredRegister('kubejs')
 
-GASES.register('methane', 0xA020F0)
+//GASES.register('methane', 0xA020F0)
+
+const $attribute = Java.loadClass('mekanism.api.chemical.gas.attribute.GasAttributes')
+const $Fuel = $attribute.Fuel
+
+GASES.register('prophane', 0xA020F0, new $Fuel(40.0,169.45F))
 
 GASES.register($EventBuses.getModEventBus('kubejs').get())
+
