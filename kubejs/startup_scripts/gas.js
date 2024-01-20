909a8349db264c6d2bb913a31e7079688856c3b7
DevDyna <andrearisorti2003@gmail.com>
2024-01-21 00:53:04 +0100

    Update gas.js

--- kubejs/startup_scripts/gas.js
+++ kubejs/startup_scripts/gas.js
@@ -4,19 +4,28 @@ const $GasDeferredRegister = Java.loadClass('mekanism.common.registration.impl.G
 const GASES = new $GasDeferredRegister('kubejs')
 
 GASES.register('methane', 0xA020F0)
-
+/* 
 const $attribute = Java.loadClass('mekanism.api.chemical.gas.attribute.GasAttributes')
 const $float = Java.loadClass('mekanism.api.math.FloatingLong')
 const $Fuel = $attribute.Fuel
-const $sup = Java.loadClass('java.util.function.IntSupplier')
-const $color = Java.loadClass('mekanism.client.SpecialColors')
+
 const $value = $float.create(100.0)
-const $screen = Java.loadClass('mekanism.client.gui.element.text.GuiTextField')
+ */
+/* function supplier(value) {
+    // Return a function that acts as a boolean supplier
+    return function() {
+        return value;
+    };
+} */
+/* 
+Java.loadClass('java.util.function.IntSupplier')
 
-//const $realcolor = Java.loadClass('mekanism.common.lib.Color')
-//const $intsup = $screen.GuiTextField.DARK_SCREEN_COLOR /* = $realcolor.argb(100).darken(0.4).argb(); */
+  const sup = new IntSupplier
 
-//GASES.register('prophane', 0xA020F0, new $Fuel(256,$value))
+	function sup = () -> 256;
 
+
+GASES.register('prophane', 0xA020F0, new $Fuel(sup,$value))
+ */
 GASES.register($EventBuses.getModEventBus('kubejs').get())
 
