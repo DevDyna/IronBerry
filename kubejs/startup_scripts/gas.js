a6c7ec535bd17ea88980859e030e159c43e16c83
DevDyna <andrearisorti2003@gmail.com>
2024-01-20 15:20:40 +0100

    Update gas.js

--- kubejs/startup_scripts/gas.js
+++ kubejs/startup_scripts/gas.js
@@ -3,12 +3,20 @@ const $GasDeferredRegister = Java.loadClass('mekanism.common.registration.impl.G
 
 const GASES = new $GasDeferredRegister('kubejs')
 
-//GASES.register('methane', 0xA020F0)
+GASES.register('methane', 0xA020F0)
 
 const $attribute = Java.loadClass('mekanism.api.chemical.gas.attribute.GasAttributes')
+const $float = Java.loadClass('mekanism.api.math.FloatingLong')
 const $Fuel = $attribute.Fuel
+const $sup = Java.loadClass('java.util.function.IntSupplier')
+const $color = Java.loadClass('mekanism.client.SpecialColors')
+const $value = $float.create(100.0)
+const $screen = Java.loadClass('mekanism.client.gui.element.text.GuiTextField')
 
-GASES.register('prophane', 0xA020F0, new $Fuel(40.0,169.45F))
+//const $realcolor = Java.loadClass('mekanism.common.lib.Color')
+//const $intsup = $screen.GuiTextField.DARK_SCREEN_COLOR /* = $realcolor.argb(100).darken(0.4).argb(); */
+
+//GASES.register('prophane', 0xA020F0, new $Fuel(256,$value))
 
 GASES.register($EventBuses.getModEventBus('kubejs').get())
 
