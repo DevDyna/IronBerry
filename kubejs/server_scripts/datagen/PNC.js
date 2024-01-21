af098a5d943ba1f51b1846e4f5e3c28c103da84e
DevDyna <andrearisorti2003@gmail.com>
2024-01-21 23:54:08 +0100

    Update PNC.js

--- kubejs/server_scripts/datagen/PNC.js
+++ kubejs/server_scripts/datagen/PNC.js
@@ -4,7 +4,7 @@ ServerEvents.highPriorityData(event => {
         event.addJson(`pneumaticcraft:recipes/pneumaticcraft_fuels/${data}`, {})
     })
 
-
+//default x1 | x1
         event.addJson(`pneumaticcraft:recipes/pneumaticcraft_fuels/water`, {
   "type": "pneumaticcraft:fuel_quality",
   "air_per_bucket": 1000,
@@ -15,5 +15,30 @@ ServerEvents.highPriorityData(event => {
     "tag": "ironberry:chad_water"
   }
 })
+
+// x4 speed | /4 efficiency
+        event.addJson(`pneumaticcraft:recipes/pneumaticcraft_fuels/biodiesel`, {
+  "type": "pneumaticcraft:fuel_quality",
+  "air_per_bucket": 500,
+  "burn_rate": 4.0,
+  "fluid": {
+	"type": "pneumaticcraft:fluid",
+    "amount": 1000,
+    "tag": "ironberry:chad_diesel"
+  }
+})
+
+// x4 efficiency | /4 speed
+        event.addJson(`pneumaticcraft:recipes/pneumaticcraft_fuels/ethanol`, {
+  "type": "pneumaticcraft:fuel_quality",
+  "air_per_bucket": 4000,
+  "burn_rate": 0.25,
+  "fluid": {
+	"type": "pneumaticcraft:fluid",
+    "amount": 1000,
+    "tag": "ironberry:chad_ethanol"
+  }
+})
+
 
 })
