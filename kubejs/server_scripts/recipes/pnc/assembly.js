d2d72891c93f1f218a979be9dbc12a68f55a76f5
DevDyna <andrearisorti2003@gmail.com>
2024-01-25 00:12:44 +0100

    Update assembly.js

--- kubejs/server_scripts/recipes/pnc/assembly.js
+++ kubejs/server_scripts/recipes/pnc/assembly.js
@@ -8,17 +8,7 @@ ServerEvents.recipes(event => {
         "program": "drill",
         "result": {
             "item": "tombstone:gift",
-            "nbt": '{Items:[{Count:1,id:"tombstone:gift",tag:{
-				Items:[{Count:1,id:"tombstone:gift",tag:{
-					Items:[{Count:1,id:"tombstone:gift",tag:{
-						Items:[{Count:1,id:"tombstone:gift",tag:{
-							Items:[{Count:1,id:"tombstone:gift",tag:{
-								Items:[{Count:1,id:"tombstone:gift",tag:{
-									Items:[{Count:1,id:"tombstone:gift",tag:{
-										Items:[{Count:1,id:"tombstone:gift",tag:{
-											Items:[{Count:1,id:"tombstone:gift",tag:{
-												Items:[{Count:1,id:"tombstone:gift",tag:{
-													Items:[{Count:1,id:"minecraft:player_head",tag:{SkullOwner:"DevDyna"}}]}}]}}]}}]}}]}}]}}]}}]}}]}}]}}]}'
+            "nbt": '{Items:[{Count:1,id:"tombstone:gift",tag:{Items:[{Count:1,id:"tombstone:gift",tag:{Items:[{Count:1,id:"tombstone:gift",tag:{Items:[{Count:1,id:"tombstone:gift",tag:{Items:[{Count:1,id:"tombstone:gift",tag:{Items:[{Count:1,id:"tombstone:gift",tag:{Items:[{Count:1,id:"tombstone:gift",tag:{Items:[{Count:1,id:"tombstone:gift",tag:{Items:[{Count:1,id:"tombstone:gift",tag:{Items:[{Count:1,id:"tombstone:gift",tag:{Items:[{Count:1,id:"minecraft:player_head",tag:{SkullOwner:"DevDyna"}}]}}]}}]}}]}}]}}]}}]}}]}}]}}]}}]}'
         }
     })
     //-----------------------------------------------------------------------------------------------------------//
