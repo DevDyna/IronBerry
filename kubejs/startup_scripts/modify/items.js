81769eecc45c3be5e6eadcdbd21f745659ebbc4c
DevDyna <andrearisorti2003@gmail.com>
2024-01-25 00:12:34 +0100

    Update items.js

--- kubejs/startup_scripts/modify/items.js
+++ kubejs/startup_scripts/modify/items.js
@@ -1,13 +1,15 @@
 ItemEvents.modification(event => {
 
     event.modify('rootsclassic:fruit_salad', item => {
-        item.foodProperties = food => {
             item.maxStackSize = 64
-        }
+
     })
 	
 	event.modify('homespun:ironberry_juice_bottle',item=>{
 		item.maxStackSize = 64
 	})
+
+
+
 
 })
