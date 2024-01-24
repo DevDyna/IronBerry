9ad858068db0219bf95020b971609aa5545d0c27
DevDyna <andrearisorti2003@gmail.com>
2024-01-25 00:15:37 +0100

    Update amadron.js

--- kubejs/server_scripts/datagen/pnc/amadron.js
+++ kubejs/server_scripts/datagen/pnc/amadron.js
@@ -33,7 +33,6 @@ birds.forEach(egg=>{
 })
 
 trade(3,'item','exoticbirds:phoenix_egg',1,'item','thermal:iron_coin',16,false)
-
 trade(0,'item','thermal:iron_coin',1,'item','minecraft:iron_ingot',4,true)
 trade(0,'item','thermal:iron_coin',1,'item','minecraft:coal',8,true)
 trade(0,'item','thermal:iron_coin',1,'item','homespun:ironberry_juice_bottle',16,true)
@@ -41,7 +40,48 @@ trade(0,'item','thermal:iron_coin',1,'item','minecraft:dirt',64,true)
 trade(0,'item','thermal:iron_coin',1,'item','minecraft:clay_ball',64,true)
 trade(0,'item','thermal:iron_coin',1,'fluid','homespun:ironberry_juice',4000,true)
 
+function trade_item_item(rarity,input,in_count,output,out_count,bool){
+	trade(rarity,'item',input,in_count,'item',output,out_count,bool)
+}
 
 
+trade_item_item(0,'thermal:electrum_coin',1,'thermal:iron_coin',16,true)
+trade_item_item(0,'thermal:iron_coin',16,'thermal:electrum_coin',1,true)
+
+
+function ironC(rarity,coin,output,count_out,bool){
+	trade_item_item(rarity,'thermal:iron_coin',coin,output,count_out,bool)
+}
+
+function elecC(rarity,coin,output,count_out,bool){
+	trade_item_item(rarity,'thermal:electrum_coin',coin,output,count_out,bool)
+}
+
+
+elecC(2,4,'minecraft:enchanted_golden_apple',1,false)
+trade_item_item(3,'tombstone:essence_of_undeath',1,'thermal:electrum_coin',1,false)
+elecC(2,24,'kubejs:processor',1,false)
+
+ironC(1,1,'inventorypets:windows_xp',1,false)
+elecC(1,1,'cyclic:teleport',2,false)
+
+let out = ['sophisticatedstorage:stack_upgrade_tier_4','ironchest:diamond_chest','bluepower:silicon_wafer', 'thermal:energy_cell_frame', 'thermal:fluid_cell_frame', 'reactive:acid_bucket', 'rootsclassic:healer_standing_stone', 'rootsclassic:vacuum_standing_stone', 'rootsclassic:repulsor_standing_stone', 'rootsclassic:accelerator_standing_stone', 'rootsclassic:entangler_standing_stone', 'energymeter:meter', 'rootsclassic:grower_standing_stone', 'rootsclassic:igniter_standing_stone', 'tombstone:lollipop', 'farmersdelight:stove', 'bluepower:cagelamp_rgb', 'exoticbirds:phoenix_egg']
+out.forEach(item=>{
+	elecC(3,1,item,1,false)
+})
+
+elecC(1,4,'tombstone:soul_receptacle',1,false)
+elecC(3,8,'prettypipes:pipe',64,false)
+elecC(2,1,'supplementaries:soap',4,false)
+ironC(1,1,'farmersdelight:chicken_sandwich',2,false)
+ironC(1,1,'farmersdelight:hamburger',2,false)
+ironC(1,1,'farmersdelight:egg_sandwich',2,false)
+
+ironC(1,1,'cyclic:ender_fishing',1,false)
+ironC(1,1,'reactive:displacer',1,false)
+ironC(1,1,'quark:forgotten_hat',1,false)
+
+elecC(1,4,'sculktransporting:speed_modifier_tier_4',1,false)
+elecC(1,4,'sculktransporting:quantity_modifier_tier_3',1,false)
 
 })
