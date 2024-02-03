1b4d6eb961673deb0919e23bf1343facb986383e
DevDyna <andrearisorti2003@gmail.com>
2024-02-03 14:03:36 +0100

    replaced with beans

--- kubejs/server_scripts/events/block_rightClicked.js
+++ kubejs/server_scripts/events/block_rightClicked.js
@@ -138,3 +138,24 @@ BlockEvents.rightClicked(event => {
         }
     })
 })
+//############################### GRAVE GUARDIAN ##########################//
+BlockEvents.rightClicked(event => {
+
+    let item = event.getItem()
+        let inv = event.player.inventory
+
+        if (inv.count(item) && event.player.mainHandItem == 'kubejs:grave_spawn') {
+            event.player.swing()
+                    //Utils.server.runCommandSilent(`/summon tombstone:grave_guardian ${x} ${y} ${z}`)
+                    let compost = event.level.createEntity('tombstone:grave_guardian')
+                        compost.y = event.block.y + 1
+                        compost.x = event.block.x + 0.5
+                        compost.z = event.block.z + 0.5
+						compost.mergeNbt({})
+                        compost.spawn()
+
+                        if (!event.player.isCreative()) {
+                            inv.extractItem(inv.find('kubejs:grave_spawn'), 1, false)
+                        }
+        }
+})
--- kubejs/server_scripts/events/item_rightClicked.js
+++ kubejs/server_scripts/events/item_rightClicked.js
@@ -1,24 +1,3 @@
-//############################### GRAVE GUARDIAN ##########################//
-ItemEvents.rightClicked('kubejs:grave_spawn', event => {
-
-    let item = event.getItem()
-        let inv = event.player.inventory
-
-        if (inv.count(item)) {
-            event.player.swing()
-            let ray = event.player.rayTrace(4.5).block
-                let x = ray.x
-                let y = ray.y + 1
-                let z = ray.z
-                if (event.level.getBlock(x, y, z) == 'minecraft:air' && event.level.getBlock(x, y + 1, z) == 'minecraft:air') {
-                    Utils.server.runCommandSilent(`/summon tombstone:grave_guardian ${x} ${y} ${z}`)
-                    if (!event.player.isCreative()) {
-                        inv.extractItem(inv.find('kubejs:grave_spawn'), 1, false)
-                    }
-                }
-
-        }
-})
 //############################### STRANGE SCROLL TO EFFECT SCROLL ##########################//
 ItemEvents.rightClicked('tombstone:strange_scroll', event => {
     let potion = event.player.getPotionEffects().getActive()
@@ -59,52 +38,52 @@ ItemEvents.rightClicked('tombstone:tablet_of_cupidity', event => {
 
 })
 
-    //############################### ILLUMINATI! ##########################//
-    ItemEvents.rightClicked('kubejs:illuminati', event => {
-        try {
-            if (event.player.inventory.getItem(event.player.inventory.find('tiab:time_in_a_bottle')) == 'tiab:time_in_a_bottle') {
-                event.player.swing()
-                if (!event.player.isCreative())
-                    event.player.addItemCooldown(event.item, 200)
-                    let random = 0
-                        if (rnd(0, 10) == 0) {
-
-                            random = rnd(12000, 48000)
-
-                                let min = Math.floor(Math.floor(random / 20) / 60)
-                                let sec = Math.floor(Math.floor(random / 20) - Math.floor((random / 60) / 20) * 60)
-                                if (sec <= 9) {
-                                    sec = "0" + sec
-                                }
-                                let star = String.fromCharCode(9733)
-                                event.player.notify(Notification.make(e => {
-                                        e.text = [Text.of(star).gold(), " Added " + min + ":" + sec + '! ', Text.of(star).gold()]
-                                        e.outlineColor = '#016055'
-                                            e.backgroundColor = '#40E862'
-                                            e.borderColor = '#267623'
-                                    }))
-
-                                Utils.server.runCommandSilent(`/execute at ${event.player.uuid} run playsound inventorypets:illuminati_confirmed block @p ~ ~ ~`)
-                        }
-                        if (random == 0) {
-                            Utils.server.runCommandSilent(`/execute at ${event.player.uuid} run playsound inventorypets:illuminati block @p ~ ~ ~`)
-                        }
-
-                        event.player.inventory.getItem(event.player.inventory.find('tiab:time_in_a_bottle')).nbt.storedTime += random
-                        event.player.inventory.getItem(event.player.inventory.find('tiab:time_in_a_bottle')).nbt.totalAccumulatedTime += random
-
-            }
-        } catch (e) {
+//############################### ILLUMINATI! ##########################//
+ItemEvents.rightClicked('kubejs:illuminati', event => {
+    try {
+        if (event.player.inventory.getItem(event.player.inventory.find('tiab:time_in_a_bottle')) == 'tiab:time_in_a_bottle') {
             event.player.swing()
             if (!event.player.isCreative())
-                event.player.addItemCooldown(event.item, 140)
-                event.player.notify(Notification.make(e => {
-                        e.text = [Text.of("Missing")]
-                        e.icon = 'ironberry:textures/item/tiab.png' //item texture fix
-                            e.outlineColor = '#0009FF'
-                            e.backgroundColor = '#03CDFF'
-                            e.borderColor = '#039BFF'
-                    }))
+                event.player.addItemCooldown(event.item, 200)
+                let random = 0
+                    if (rnd(0, 10) == 0) {
+
+                        random = rnd(12000, 48000)
+
+                            let min = Math.floor(Math.floor(random / 20) / 60)
+                            let sec = Math.floor(Math.floor(random / 20) - Math.floor((random / 60) / 20) * 60)
+                            if (sec <= 9) {
+                                sec = "0" + sec
+                            }
+                            let star = String.fromCharCode(9733)
+                            event.player.notify(Notification.make(e => {
+                                    e.text = [Text.of(star).gold(), " Added " + min + ":" + sec + '! ', Text.of(star).gold()]
+                                    e.outlineColor = '#016055'
+                                        e.backgroundColor = '#40E862'
+                                        e.borderColor = '#267623'
+                                }))
+
+                            Utils.server.runCommandSilent(`/execute at ${event.player.uuid} run playsound inventorypets:illuminati_confirmed block @p ~ ~ ~`)
+                    }
+                    if (random == 0) {
+                        Utils.server.runCommandSilent(`/execute at ${event.player.uuid} run playsound inventorypets:illuminati block @p ~ ~ ~`)
+                    }
+
+                    event.player.inventory.getItem(event.player.inventory.find('tiab:time_in_a_bottle')).nbt.storedTime += random
+                    event.player.inventory.getItem(event.player.inventory.find('tiab:time_in_a_bottle')).nbt.totalAccumulatedTime += random
+
         }
-    })
-    //###############################  ##########################//
+    } catch (e) {
+        event.player.swing()
+        if (!event.player.isCreative())
+            event.player.addItemCooldown(event.item, 140)
+            event.player.notify(Notification.make(e => {
+                    e.text = [Text.of("Missing")]
+                    e.icon = 'ironberry:textures/item/tiab.png' //item texture fix
+                        e.outlineColor = '#0009FF'
+                        e.backgroundColor = '#03CDFF'
+                        e.borderColor = '#039BFF'
+                }))
+    }
+})
+//###############################  ##########################//
