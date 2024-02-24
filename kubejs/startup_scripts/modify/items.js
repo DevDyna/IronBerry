9363f647480c9fc17ec613a32e5376df83e331b1
DevDyna <andrearisorti2003@gmail.com>
2024-02-24 13:52:24 +0100

    food tweak

--- kubejs/startup_scripts/modify/items.js
+++ kubejs/startup_scripts/modify/items.js
@@ -2,7 +2,9 @@ ItemEvents.modification(event => {
 
     event.modify('rootsclassic:fruit_salad', item => {
             item.maxStackSize = 64
-
+			item.foodProperties = food => {
+				food.fastToEat(true)
+			}
     })
 	
 	event.modify('homespun:ironberry_juice_bottle',item=>{
@@ -11,8 +13,20 @@ ItemEvents.modification(event => {
 
     event.modify('rootsclassic:rooty_stew',item=>{
 		item.maxStackSize = 64
+		item.foodProperties = food => {
+			food.hunger(20)
+			food.saturation(1)
+			food.fastToEat(true)
+		}
 	})
     
+	event.modify('minecraft:bread',item=>{
+		item.foodProperties = food => {
+			food.hunger(10)
+			food.saturation(2)
+			food.fastToEat(true)
+		}
+	})
 
 
 })
