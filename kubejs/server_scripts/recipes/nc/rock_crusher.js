41d6bcffdb3ee3c9c81ad3e9595d8c7cbc3f5e9b
DevDyna <andrearisorti2003@gmail.com>
2024-03-05 14:04:49 +0100

    lupenio

--- kubejs/server_scripts/recipes/nc/rock_crusher.js
+++ kubejs/server_scripts/recipes/nc/rock_crusher.js
@@ -14,10 +14,10 @@ function crusher(item_in,icount,item_out,ocount){
   "powerModifier": (global.nc.energy),
   "radiation": (global.nc.rad),
   "timeModifier": (global.nc.speed)
-}).id(RegX(input_item + '_' + item_out, '_'))
+}).id(RegX(item_in + '_' + item_out, '_'))
 }
 
 crusher('quark:sturdy_stone',1,['tombstone:white_marble', 'tombstone:dark_marble'],[1,1])
-//crusher('minecraft:deepslate',1,['kubejs:lupenio','minecraft:diamond','minecraft:emerald'],[1,1,1])
+crusher('minecraft:deepslate',1,['kubejs:lupenio', 'minecraft:diamond'],[1,1])
 
 })
