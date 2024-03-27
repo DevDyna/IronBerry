8a7f7a163ad94986f8802d6b04fec5992dba6a02
DevDyna <andrearisorti2003@gmail.com>
2024-03-27 13:50:54 +0100

    fix cutout

---
+++ kubejs/client_scripts/cutout.js
@@ -0,0 +1,7 @@
+const $ItemBlockRenderTypes = Java.loadClass('net.minecraft.client.renderer.ItemBlockRenderTypes')
+const $RenderType = Java.loadClass('net.minecraft.client.renderer.RenderType')
+const $Blocks = Java.loadClass('net.minecraft.world.level.block.Blocks')
+
+ClientEvents.init(event => {
+  $ItemBlockRenderTypes.setRenderLayer('ironberry:block/incompleted_machine_frame', $RenderType.cutout())
+})
