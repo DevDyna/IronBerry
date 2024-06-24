0744dd6df5cdb47911bc4989417c49b9d821770d
DevDyna <andrearisorti2003@gmail.com>
2024-06-25 00:38:31 +0200

    formatted and added biomancy CountRangeOrUniform

--- kubejs/server_scripts/codex.js
+++ kubejs/server_scripts/codex.js
@@ -6,85 +6,89 @@ return string.replace(/:/g, '_')
 } */
 //cut all over the wall and return (wall = char not int)
 function wall(string, wall) {
-    let part = string.split(wall)
-        return part[0]
+  let part = string.split(wall);
+  return part[0];
 }
 //cut all before index (index = int not chat)
 function cut(string, index) {
-    return string.substring(index);
+  return string.substring(index);
 }
 //check if value is a tag or item and return as json
 function ItemOrTag(value) {
-    if (value.charAt(0) == '#') {
-        return {
-            "tag": cut(value, 1)
-        }
-    } else {
-        return JsonMakeItem(value)
-    }
+  if (value.charAt(0) == "#") {
+    return {
+      tag: cut(value, 1),
+    };
+  } else {
+    return JsonMakeItem(value);
+  }
 }
 //check if value is a tag or item and return as json + count value
 function ItemOrTagAndCount(value, count) {
-    if (count < 2) {
-        return ItemOrTag(value)
-    } else if (value.charAt(0) === '#') {
-        return {
-            "tag": cut(value, 1),
-            "count": count
-        }
-    } else {
-        return ItemAndCount(value, count)
-    }
-
+  if (count < 2) {
+    return ItemOrTag(value);
+  } else if (value.charAt(0) === "#") {
+    return {
+      tag: cut(value, 1),
+      count: count,
+    };
+  } else {
+    return ItemAndCount(value, count);
+  }
 }
 //return as json + count value
 function ItemAndCount(value, count) {
-        if (count < 2) {
-        return JsonMakeItem(value)
-    } else return {
-        "item": value,
-        "count": count
-    }
+  if (count < 2) {
+    return JsonMakeItem(value);
+  } else
+    return {
+      item: value,
+      count: count,
+    };
 }
 //return as json + count value
 function ItemAndChance(value, chance) {
-    if (chance == 1.0) {
-    return JsonMakeItem(value)
-} else return {
-    "item": value,
-    "chance": chance
-}
+  if (chance == 1.0) {
+    return JsonMakeItem(value);
+  } else
+    return {
+      item: value,
+      chance: chance,
+    };
 }
 //return as json + count value
 function FluidAndCount(value, count) {
-    if (count < 2) {
-    return JsonMakeFluid(value)
-} else return {
-    "fluid": value,
-    "amount": count
-}
+  if (count < 2) {
+    return JsonMakeFluid(value);
+  } else
+    return {
+      fluid: value,
+      amount: count,
+    };
 }
 //json creator
 function JsonMakeItem(value) {
-    return {
-        "item": value
-    }
+  return {
+    item: value,
+  };
 }
 //json creator
 function JsonMakeFluid(value) {
-    return {
-        "fluid": value
-    }
+  return {
+    fluid: value,
+  };
 }
-
 
 //cut all except the string between the walls and return
 function doublewall(string, start, end) {
-    try {
-        return string.substring(string.indexOf(start) + start.length, string.indexOf(end, string.indexOf(start)))
-    } catch (error) {
-        return null
-    }
+  try {
+    return string.substring(
+      string.indexOf(start) + start.length,
+      string.indexOf(end, string.indexOf(start))
+    );
+  } catch (error) {
+    return null;
+  }
 }
 //RND better
 /*function rnd(min, max) {
@@ -98,46 +102,59 @@ function doublewall(string, start, end) {
 }*/
 //forEach better
 function Feach(e, group) {
-    let found = false
-        group.forEach((id) => {
-            if (e === id) {
-                found = true
-            }
-        })
-        return found
+  let found = false;
+  group.forEach((id) => {
+    if (e === id) {
+      found = true;
+    }
+  });
+  return found;
 }
 //notification easy
-function ENotify(event, text, itemIcon, outlineColor, backgroundColor, borderColor) {
-
-    event.player.notify(Notification.make(e => {
-            e.text = text
-                e.itemIcon = itemIcon
-                e.outlineColor = outlineColor
-                e.backgroundColor = backgroundColor
-                e.borderColor = borderColor
-        }))
-
+function ENotify(
+  event,
+  text,
+  itemIcon,
+  outlineColor,
+  backgroundColor,
+  borderColor
+) {
+  event.player.notify(
+    Notification.make((e) => {
+      e.text = text;
+      e.itemIcon = itemIcon;
+      e.outlineColor = outlineColor;
+      e.backgroundColor = backgroundColor;
+      e.borderColor = borderColor;
+    })
+  );
 }
 //----------------------TAG-TOOLS-------------------------//
 //remove the tags from any of items
 function NoTag(event, list) {
-    for (let i = 0; i < list.length; i++) {
-        event.removeAll(list[i])
-    }
+  for (let i = 0; i < list.length; i++) {
+    event.removeAll(list[i]);
+  }
 }
 //add all tag items on a variable
 function TagPush(event, base, variable) {
-    console.log(variable)
-    event.get(base).getObjectIds().forEach(pot => {
-        variable.push(pot)
-    })
-    return variable
+  console.log(variable);
+  event
+    .get(base)
+    .getObjectIds()
+    .forEach((pot) => {
+      variable.push(pot);
+    });
+  return variable;
 }
 //remove a tag based on another tag
 function TagRemoveTag(event, base, toRemove) {
-    event.get(base).getObjectIds().forEach(pot => {
-        event.remove(toRemove, pot)
-    })
+  event
+    .get(base)
+    .getObjectIds()
+    .forEach((pot) => {
+      event.remove(toRemove, pot);
+    });
 }
 //-------------------------------------------------------//
 /*function roll_time(chance) { // more = common | less = rare
@@ -151,18 +168,37 @@ function TagRemoveTag(event, base, toRemove) {
 }*/
 
 function RegX(string, replace) {
-    if (typeof optionalValue != 'undefined') { //if replace still defined
-        return string.replace(/[^a-zA-Z0-9_ ]/g, replace)
-    } else {
-        return string.replace(/[^a-zA-Z0-9_ ]/g, '')
-    }
-
+  if (typeof optionalValue != "undefined") {
+    //if replace still defined
+    return string.replace(/[^a-zA-Z0-9_ ]/g, replace);
+  } else {
+    return string.replace(/[^a-zA-Z0-9_ ]/g, "");
+  }
 }
 //rnd bool again
 function rnd_booldozer(min, max) {
-    if (rnd(min, max) == max) {
-        return true
-    } else {
-        return false
-    }
+  if (rnd(min, max) == max) {
+    return true;
+  } else {
+    return false;
+  }
+}
+/**
+ * Rangeominator
+ * @param {number} input like 10 or 0-1 but NOT NEGATIVES!
+ * @returns { {uniform} | {constant}  }
+ */
+function CountRangeOrUniform(input) {
+  input = input.toString().trim();
+  if (input.includes("-")) {
+    let parts = input.split("-").map((part) => part.trim());
+
+    return {
+      type: "uniform",
+      min: parseFloat(parts[0]),
+      max: parseFloat(parts[1]),
+    };
+  } else {
+    return { type: "constant", value: parseFloat(input) };
+  }
 }
