975ba599cb8a04d64c4f2216f0cc7990e909f43b
DevDyna <andrearisorti2003@gmail.com>
2024-11-11 23:32:21 +0100

    Update altar.js

--- kubejs/server_scripts/recipes/extendedcrafting/altar.js
+++ kubejs/server_scripts/recipes/extendedcrafting/altar.js
@@ -1,151 +1,147 @@
-ServerEvents.recipes(event=>{
-    event.custom({
-        "type": "extendedcrafting:combination",
-        "powerCost": 2500000,
-        "input": {
-          "item": "kubejs:incompleted_machine_frame"
-        },
-        "ingredients": [
-          {
-            "item": "thoriumreactors:turbine_casing"
-          },
-          {
-            "item": "thoriumreactors:turbine_casing"
-          },
-          {
-            "item": 'nuclearcraft:plate_extreme'
-          },
-          {
-            "item": "thoriumreactors:reactor_casing"
-          },
-          {
-            "item": "thoriumreactors:turbine_casing"
-          },
-          {
-            "item": "thoriumreactors:turbine_casing"
-          },
-          {
-            "item": 'nuclearcraft:plate_extreme'
-          },
-          {
-            "item": "thoriumreactors:turbine_casing"
-          },
-          {
-            "item": "thoriumreactors:turbine_casing"
-          },
-          {
-            "item": "thoriumreactors:reactor_casing"
-          },
-          {
-            "item": "thoriumreactors:reactor_casing"
-          },
-          {
-            "item": 'nuclearcraft:plate_extreme'
-          },
-          {
-            "item": "thoriumreactors:reactor_casing"
-          },
-          {
-            "item": "thoriumreactors:reactor_casing"
-          },
-          {
-            "item": "thoriumreactors:turbine_casing"
-          },
-          {
-            "item": 'nuclearcraft:plate_extreme'
-          },
-          {
-            "item": "thoriumreactors:reactor_casing"
-          },
-          {
-            "item": "thoriumreactors:reactor_casing"
-          },
-          {
-            "item": "thoriumreactors:turbine_casing"
-          },
-          {
-            "item": "thoriumreactors:reactor_casing"
-          }
-        ],
-        "result": {
-          "item": 'thermal:machine_frame'
-        }
-      })
+ServerEvents.recipes((event) => {
+  event.custom({
+    type: "extendedcrafting:combination",
+    powerCost: 2500000,
+    input: {
+      item: "kubejs:incompleted_machine_frame",
+    },
+    ingredients: [
+      {
+        item: "thoriumreactors:turbine_casing",
+      },
+      {
+        item: "thoriumreactors:turbine_casing",
+      },
+      {
+        item: "nuclearcraft:plate_extreme",
+      },
+      {
+        item: "thoriumreactors:reactor_casing",
+      },
+      {
+        item: "thoriumreactors:turbine_casing",
+      },
+      {
+        item: "thoriumreactors:turbine_casing",
+      },
+      {
+        item: "nuclearcraft:plate_extreme",
+      },
+      {
+        item: "thoriumreactors:turbine_casing",
+      },
+      {
+        item: "thoriumreactors:turbine_casing",
+      },
+      {
+        item: "thoriumreactors:reactor_casing",
+      },
+      {
+        item: "thoriumreactors:reactor_casing",
+      },
+      {
+        item: "nuclearcraft:plate_extreme",
+      },
+      {
+        item: "thoriumreactors:reactor_casing",
+      },
+      {
+        item: "thoriumreactors:reactor_casing",
+      },
+      {
+        item: "thoriumreactors:turbine_casing",
+      },
+      {
+        item: "nuclearcraft:plate_extreme",
+      },
+      {
+        item: "thoriumreactors:reactor_casing",
+      },
+      {
+        item: "thoriumreactors:reactor_casing",
+      },
+      {
+        item: "thoriumreactors:turbine_casing",
+      },
+      {
+        item: "thoriumreactors:reactor_casing",
+      },
+    ],
+    result: {
+      item: "thermal:machine_frame",
+    },
+  });
 
-      event.custom({
-        "type": "extendedcrafting:combination",
-        "powerCost": 2500000,
-        "input": {
-          "item": "kubejs:blank"
-        },
-        "ingredients": [
-          {
-            "item": "thoriumreactors:machine_casing"
-          },
-          {
-            "item": "thoriumreactors:machine_casing"
-          },
-          {
-            "item": "nuclearcraft:plate_elite"
-          },
-          {
-            "item": "thoriumreactors:machine_casing"
-          },
-          {
-            "item": "thoriumreactors:machine_casing"
-          },
-          {
-            "item": "thoriumreactors:machine_casing"
-          },
-          {
-            "item": "nuclearcraft:plate_elite"
-          },
-          {
-            "item": "thoriumreactors:machine_casing"
-          },
-          {
-            "item": "thoriumreactors:machine_casing"
-          },
-          {
-            "item": "thoriumreactors:machine_casing"
-          },
-          {
-            "item": "thoriumreactors:machine_casing"
-          },
-          {
-            "item": "nuclearcraft:plate_elite"
-          },
-          {
-            "item": "thoriumreactors:machine_casing"
-          },
-          {
-            "item": "thoriumreactors:machine_casing"
-          },
-          {
-            "item": "thoriumreactors:machine_casing"
-          },
-          {
-            "item": "nuclearcraft:plate_elite"
-          },
-          {
-            "item": "thoriumreactors:machine_casing"
-          },
-          {
-            "item": "thoriumreactors:machine_casing"
-          },
-          {
-            "item": "thoriumreactors:machine_casing"
-          },
-          {
-            "item": "thoriumreactors:machine_casing"
-          }
-        ],
-        "result": {
-          "item": 'kubejs:incompleted_machine_frame'
-        }
-      })
-
-
-
-      
-})
+  event.custom({
+    type: "extendedcrafting:combination",
+    powerCost: 2500000,
+    input: {
+      item: "thoriumreactors:thermal_conductor",
+    },
+    ingredients: [
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+      {
+        item: "nuclearcraft:plate_elite",
+      },
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+      {
+        item: "nuclearcraft:plate_elite",
+      },
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+      {
+        item: "nuclearcraft:plate_elite",
+      },
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+      {
+        item: "nuclearcraft:plate_elite",
+      },
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+      {
+        item: "thoriumreactors:machine_casing",
+      },
+    ],
+    result: {
+      item: "kubejs:incompleted_machine_frame",
+    },
+  });
+});
