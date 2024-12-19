fe1cb3b90e46480f8b2809f45548fd25ab53135e
DevDyna <andrearisorti2003@gmail.com>
2024-12-19 13:20:42 +0100

    Update compressor.js

--- kubejs/server_scripts/recipes/extendedcrafting/compressor.js
+++ kubejs/server_scripts/recipes/extendedcrafting/compressor.js
@@ -1,33 +1,68 @@
-ServerEvents.recipes(event => {
+ServerEvents.recipes((event) => {
+  event.custom({
+    type: `extendedcrafting:compressor`,
+    powerCost: 50000.0,
+    inputCount: 16,
+    ingredient: {
+      item: "kubejs:crystaltine",
+    },
+    catalyst: {
+      item: "extendedcrafting:flux_star",
+    },
+    result: {
+      item: "extendedcrafting:crystaltine_ingot",
+    },
+  });
+
+  event.custom({
+    type: `extendedcrafting:compressor`,
+    powerCost: 50000.0,
+    inputCount: 16,
+    ingredient: {
+      item: "extendedcrafting:crystaltine_ingot",
+    },
+    catalyst: {
+      item: "extendedcrafting:flux_star",
+    },
+    result: {
+      item: "extendedcrafting:singularity",
+      nbt: '{Id:"extendedcrafting:crystaltine"}',
+    },
+  });
+
+  function compressor(input, output, rf, incount) {
     event.custom({
-        type: `extendedcrafting:compressor`,
-        powerCost: 50000.0,
-        inputCount: 16,
-        ingredient: {
-            item: 'kubejs:crystaltine',
-        },
-        catalyst: {
-            item: 'extendedcrafting:flux_star',
-        },
-        result: {
-            item: 'extendedcrafting:crystaltine_ingot',
-        },
-    })
+      type: `extendedcrafting:compressor`,
+      powerCost: rf,
+      inputCount: 1,
+      ingredient: {
+        item: output,
+      },
+      catalyst: {
+        item: "twilightforest:uncrafting_table",
+      },
+      result: {
+        item: input,
+        count: 16,
+      },
+    });
 
     event.custom({
-        type: `extendedcrafting:compressor`,
-        powerCost: 50000.0,
-        inputCount: 16,
-        ingredient: {
-            item: 'extendedcrafting:crystaltine_ingot',
-        },
-        catalyst: {
-            item: 'extendedcrafting:flux_star',
-        },
-        result: {
-            item: 'extendedcrafting:singularity', 
-            nbt: '{Id:"extendedcrafting:crystaltine"}'
-        },
-    })
+      type: `extendedcrafting:compressor`,
+      powerCost: rf,
+      inputCount: 16,
+      ingredient: {
+        item: input,
+      },
+      catalyst: {
+        item: "minecraft:crafting_table",
+      },
+      result: { item: output },
+    });
+  }
 
-})
+  compressor("thermal:iron_coin", "thermal:electrum_coin", 10000);
+  compressor("thermal:electrum_coin", "thermal:lead_coin", 10000);
+  compressor("thermal:lead_coin", "thermal:enderium_coin", 10000);
+
+});
